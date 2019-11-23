b561ae0249e5c23cab7908f0361d1873c23283da
Igor Trindade <igorhideki@gmail.com>
2019-11-23 16:43:27 -0400

    Add SessionController

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '7bb89e87f46871fd0f6101ce70557a09',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Feedback API running' }));
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
