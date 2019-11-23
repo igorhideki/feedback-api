1e50a392542f789fd355e313fd2319c707963e73
Igor Trindade <igorhideki@gmail.com>
2019-11-23 15:55:02 -0400

    Add UserController

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Feedback API running' }));
+
+routes.post('/users', UserController.store);
 
 export default routes;
