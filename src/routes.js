f5de9fd0a7250de510361de8f9e9bf2c338267cf
Igor Trindade <igorhideki@gmail.com>
2019-11-23 20:44:51 -0400

    Add authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -9,5 +10,7 @@ routes.get('/', (req, res) => res.json({ message: 'Feedback API running' }));
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
